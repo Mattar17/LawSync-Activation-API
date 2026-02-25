f23e381142224ac68a542ac3662b83b57978733b
Mattar17 <mattar17x@gmail.com>
2026-02-25 13:55:50 +0200

    refactor: start trial endpoint

--- src/BeginTrial.ts
+++ src/BeginTrial.ts
@@ -5,22 +5,16 @@ import signLicense from "./helpers/signLicense.js";
 export default async function BeginTrial(req: Request, res: Response) {
   try {
     const { machineId } = req.body;
-    const trialExists = await Trial.findOne({ machineId });
-    if (trialExists) {
-      return res.status(409).json({
-        success: false,
-        message: "Trial already exists",
-        data: trialExists,
-      });
+    const trial = await Trial.findOne({ machineId });
+    if (!trial) {
+      let newTrial = new Trial({ machineId });
+      await newTrial.save();
     }
 
-    let newTrial = new Trial({ machineId });
-    await newTrial.save();
-    const signedTrial = signLicense(newTrial);
     return res.status(201).json({
       success: true,
       message: "Trial started successfully",
-      data: signedTrial,
+      data: signLicense(trial!),
     });
   } catch (err) {
     return res.status(500).json({ success: false, message: "server error" });
