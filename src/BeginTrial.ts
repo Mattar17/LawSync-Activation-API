7e0993eea4bf94d1122827e09b218ed85c6d310a
Mattar17 <mattar17x@gmail.com>
2026-02-25 14:34:34 +0200

    refactor: remove license signing from trial response data

--- src/BeginTrial.ts
+++ src/BeginTrial.ts
@@ -1,6 +1,5 @@
 import { type Request, type Response } from "express";
 import Trial from "./Trial.js";
-import signLicense from "./helpers/signLicense.js";
 
 export default async function BeginTrial(req: Request, res: Response) {
   try {
@@ -13,12 +12,11 @@ export default async function BeginTrial(req: Request, res: Response) {
 
     console.log("Trial started for machineId:", machineId);
     console.log("Trial details:", trial);
-    console.log("License signed for trial:", signLicense(trial!));
 
     return res.status(201).json({
       success: true,
       message: "Trial started successfully",
-      data: signLicense(trial!),
+      data: trial,
     });
   } catch (err) {
     return res.status(500).json({ success: false, message: "server error" });
