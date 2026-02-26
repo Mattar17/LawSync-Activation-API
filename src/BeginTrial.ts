87eeb8c18b098bfe99848a91f26baa54cb7c8799
Mattar17 <mattar17x@gmail.com>
2026-02-26 02:05:53 +0200

    refactor: streamline trial creation and signing process in BeginTrial function

--- src/BeginTrial.ts
+++ src/BeginTrial.ts
@@ -5,19 +5,21 @@ import signLicense from "./helpers/signLicense.js";
 export default async function BeginTrial(req: Request, res: Response) {
   try {
     const { machineId } = req.body;
+    let newTrial;
     const trial = await Trial.findOne({ machineId });
     if (!trial) {
-      let newTrial = new Trial({ machineId });
+      newTrial = new Trial({
+        machineId,
+        type: "trial",
+        expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+      });
       await newTrial.save();
     }
-
-    console.log("Trial started for machineId:", machineId);
-    console.log("Trial details:", trial);
 
     return res.status(201).json({
       success: true,
       message: "Trial started successfully",
-      data: signLicense(trial!),
+      data: signLicense(trial! || newTrial!),
     });
   } catch (err) {
     return res.status(500).json({ success: false, message: "server error" });
