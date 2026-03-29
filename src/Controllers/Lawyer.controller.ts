c993658024d0fec5b735e8b1b6e3adb3906f817e
Mattar17 <mattar17x@gmail.com>
2026-03-29 02:32:06 +0200

    Implement authentication and authorization features with JWT, bcrypt, and middleware for admin access

--- src/Controllers/Lawyer.controller.ts
+++ src/Controllers/Lawyer.controller.ts
@@ -1,11 +1,16 @@
 import LawyerModel from "../Models/Lawyer";
 import { type Request, type Response } from "express";
 import { generateLawyerId } from "../utils/generateLawyerId.js";
+import bcrypt from "bcrypt";
 
-/**
- * @desc Get all lawyers
- */
-export const getAllLawyers = async (req: Request, res: Response) => {
+interface AuthRequest extends Request {
+  token?: {
+    admin?: boolean;
+    lawyer_token?: string;
+  };
+}
+
+export const getAllLawyers = async (req: AuthRequest, res: Response) => {
   try {
     const lawyers = await LawyerModel.find();
 
@@ -14,7 +19,7 @@ export const getAllLawyers = async (req: Request, res: Response) => {
       count: lawyers.length,
       data: lawyers,
     });
-  } catch (err) {
+  } catch {
     return res.status(500).json({
       success: false,
       message: "Server Error",
@@ -22,13 +27,9 @@ export const getAllLawyers = async (req: Request, res: Response) => {
   }
 };
 
-/**
- * @desc Get single lawyer by Token
- */
-export const getLawyerByToken = async (req: Request, res: Response) => {
+export const getLawyerByToken = async (req: AuthRequest, res: Response) => {
   try {
     const lawyer = await LawyerModel.find({ token: `${req.params.token}` });
-    console.log(lawyer);
 
     if (!lawyer.length) {
       return res.status(404).json({
@@ -41,7 +42,7 @@ export const getLawyerByToken = async (req: Request, res: Response) => {
       success: true,
       data: lawyer,
     });
-  } catch (err) {
+  } catch {
     return res.status(500).json({
       success: false,
       message: "Server Error",
@@ -49,20 +50,30 @@ export const getLawyerByToken = async (req: Request, res: Response) => {
   }
 };
 
-/**
- * @desc Create new lawyer
- */
-export const createLawyer = async (req: Request, res: Response) => {
+export const createLawyer = async (req: AuthRequest, res: Response) => {
   try {
+    console.log(req.token);
+    if (!req.token?.admin) {
+      return res.status(403).json({
+        success: false,
+        message: "Access denied, admin only",
+      });
+    }
+
     const { name } = req.body;
+
     const token: string = await generateLawyerId();
-    console.log(token);
     if (!token) throw new Error("token generation failed");
+
+    const hashedProfilePassword = await bcrypt.hash("000000", 10);
+    const hashedPortalPassword = await bcrypt.hash("000000", 10);
+
     const lawyer = await LawyerModel.create({
       token,
       name,
+      profile_password: hashedProfilePassword,
+      portal_password: hashedPortalPassword,
     });
-    console.log(lawyer);
 
     return res.status(201).json({
       success: true,
@@ -76,19 +87,9 @@ export const createLawyer = async (req: Request, res: Response) => {
   }
 };
 
-/**
- * @desc Update lawyer
- */
-export const updateLawyer = async (req: Request, res: Response) => {
+export const updateLawyer = async (req: AuthRequest, res: Response) => {
   try {
-    const lawyer = await LawyerModel.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      {
-        new: true,
-        runValidators: true,
-      },
-    );
+    const lawyer = await LawyerModel.findById(req.params.id);
 
     if (!lawyer) {
       return res.status(404).json({
@@ -96,6 +97,16 @@ export const updateLawyer = async (req: Request, res: Response) => {
         message: "Lawyer not found",
       });
     }
+
+    if (req.token?.lawyer_token !== lawyer.token) {
+      return res.status(403).json({
+        success: false,
+        message: "Access denied",
+      });
+    }
+
+    Object.assign(lawyer, req.body);
+    await lawyer.save();
 
     return res.status(200).json({
       success: true,
@@ -109,13 +120,9 @@ export const updateLawyer = async (req: Request, res: Response) => {
   }
 };
 
-/**
- * @desc Delete lawyer
- */
-export const deleteLawyer = async (req: Request, res: Response) => {
+export const deleteLawyer = async (req: AuthRequest, res: Response) => {
   try {
-    const { id } = req.params;
-    const lawyer = await LawyerModel.findByIdAndDelete(id);
+    const lawyer = await LawyerModel.findById(req.params.id);
 
     if (!lawyer) {
       return res.status(404).json({
@@ -124,14 +131,124 @@ export const deleteLawyer = async (req: Request, res: Response) => {
       });
     }
 
+    if (!req.token?.admin) {
+      return res.status(403).json({
+        success: false,
+        message: "Access denied,Admin only",
+      });
+    }
+
+    await lawyer.deleteOne();
+
     return res.status(200).json({
       success: true,
       message: "Lawyer deleted successfully",
     });
-  } catch (err) {
+  } catch {
     return res.status(500).json({
       success: false,
-      message: `${err} Server Error`,
+      message: "Server Error",
+    });
+  }
+};
+
+export const updateProfilePassword = async (
+  req: AuthRequest,
+  res: Response,
+) => {
+  try {
+    const { currentPassword, newPassword } = req.body;
+
+    const lawyer = await LawyerModel.findById(req.params.id);
+
+    if (!lawyer) {
+      return res.status(404).json({
+        success: false,
+        message: "Lawyer not found",
+      });
+    }
+
+    if (req.token?.lawyer_token !== lawyer.token) {
+      return res.status(403).json({
+        success: false,
+        message: "Access denied",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(
+      currentPassword,
+      lawyer.profile_password,
+    );
+
+    if (!isMatch) {
+      return res.status(400).json({
+        success: false,
+        message: "Current password is incorrect",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+    lawyer.profile_password = hashedPassword;
+
+    await lawyer.save();
+
+    return res.status(200).json({
+      success: true,
+      message: "Profile password updated successfully",
+    });
+  } catch (err: any) {
+    return res.status(400).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
+
+export const updatePortalPassword = async (req: AuthRequest, res: Response) => {
+  try {
+    const { profilePassword, newPortalPassword } = req.body;
+
+    const lawyer = await LawyerModel.findById(req.params.id);
+
+    if (!lawyer) {
+      return res.status(404).json({
+        success: false,
+        message: "Lawyer not found",
+      });
+    }
+
+    if (req.token?.lawyer_token !== lawyer.token) {
+      return res.status(403).json({
+        success: false,
+        message: "Access denied",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(
+      profilePassword,
+      lawyer.profile_password,
+    );
+
+    if (!isMatch) {
+      return res.status(400).json({
+        success: false,
+        message: "Profile password is incorrect",
+      });
+    }
+
+    const hashedPortalPassword = await bcrypt.hash(newPortalPassword, 10);
+    lawyer.portal_password = hashedPortalPassword;
+
+    await lawyer.save();
+
+    return res.status(200).json({
+      success: true,
+      message: "Portal password updated successfully",
+    });
+  } catch (err: any) {
+    return res.status(400).json({
+      success: false,
+      message: err.message,
     });
   }
 };
--- src/router.ts
+++ src/router.ts
@@ -3,33 +3,60 @@ import APIKeyValidation from "./middlewares/APIKeyValidator.js";
 import ValidateLicense from "./Controllers/ValidateLicense.js";
 import ActivateLicense from "./Controllers/ActivateLicense.js";
 import StartTrial from "./Controllers/BeginTrial.js";
-import CaseModel from "./Models/Case.js";
+import AdminOnly from "./middlewares/adminOnly.js";
+import verifyToken from "./middlewares/verifyToken.js";
 import {
   numberOfDownloads,
   increamentDownloads,
 } from "./Controllers/Analytics.js";
 import * as LaweryController from "./Controllers/Lawyer.controller.js";
 import { getLawyerCases } from "./Controllers/getCases.js";
+import { Login } from "./Controllers/Login.js";
 
 const router = express.Router();
+router.use(APIKeyValidation);
 
 //Activation
-router.post("/api/licenses/validate", APIKeyValidation, ValidateLicense);
-router.post("/api/licenses/activate", APIKeyValidation, ActivateLicense);
-router.post("/api/licenses/trial/start", APIKeyValidation, StartTrial);
+router.post("/api/licenses/validate", ValidateLicense);
+router.post("/api/licenses/activate", ActivateLicense);
+router.post("/api/licenses/trial/start", StartTrial);
 
 //Analytics
 router.get("/api/analytics/downloads", numberOfDownloads);
-router.patch("/api/analytics/downloads", APIKeyValidation, increamentDownloads);
+router.patch("/api/analytics/downloads", increamentDownloads);
 
 //Lawyers
 router.get("/api/lawyers/", LaweryController.getAllLawyers);
 router.get("/api/lawyers/:token", LaweryController.getLawyerByToken);
-router.post("/api/lawyers/", LaweryController.createLawyer); //Admin only
-router.put("/api/lawyers/:id", LaweryController.updateLawyer); //Lawyer profile (token required)
-router.delete("/api/lawyers/:id", LaweryController.deleteLawyer); //Admin only
+router.post(
+  "/api/lawyers/",
+  verifyToken,
+  AdminOnly,
+  LaweryController.createLawyer,
+);
+router.put("/api/lawyers/:id", verifyToken, LaweryController.updateLawyer);
+router.delete(
+  "/api/lawyers/:id",
+  verifyToken,
+  AdminOnly,
+  LaweryController.deleteLawyer,
+);
+router.post(
+  "/api/lawyers/:id/update-profile-password",
+  verifyToken,
+  LaweryController.updateProfilePassword,
+);
+
+router.post(
+  "/api/lawyers/:id/update-portal-password",
+  verifyToken,
+  LaweryController.updatePortalPassword,
+);
 
 //Lawyers
 router.get("/api/cases/:token", getLawyerCases);
+
+//Login
+router.post("/api/login", Login);
 
 export default router;
